2753d1c4bcd5a132f4168401b4df14632133c822
Naresh <naresh.mmmec@gmail.com>
2020-08-31 11:27:59 +0530

    Angular Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { StudentregistrationComponent } from './studentregistration/studentregistration.component';
+import { StudentdetailsComponent } from './studentdetails/studentdetails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path : '',redirectTo:'student',pathMatch:'full'},
+  {path :'student',component:StudentComponent},
+  {path: 'employee',component:EmployeeComponent},
+  // {path: 'studentregistration',component:StudentregistrationComponent},
+  {path: 'student', 
+  children : [
+    {path:'',component:StudentComponent},
+    {path :'studentregistration',component:StudentregistrationComponent},
+    {path:'studentdetails',component:StudentdetailsComponent}
+  ],
+  },
+  {path:'**', component:PagenotfoundComponent},
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { StudentregistrationComponent } from './studentregistration/studentregistration.component';
+import { StudentdetailsComponent } from './studentdetails/studentdetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    EmployeeComponent,
+    PagenotfoundComponent,
+    StudentregistrationComponent,
+    StudentdetailsComponent
   ],
   imports: [
     BrowserModule,
