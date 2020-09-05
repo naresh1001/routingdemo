9c26fd0ad30db36b3adb6c615018dae4f7109711
Naresh <naresh.mmmec@gmail.com>
2020-09-05 11:25:55 +0530

    binding with ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routingdemo';
+  students:any[] = [
+    {"id":123,"name":"Nirmit","dept":"MCA"},
+    {"id":124,"name":"Rajat","dept":"MBA"},
+    {"id":125,"name":"Ayushi","dept":"MTech"},
+    {"id":126,"name":"Aakash","dept":"MCA"},
+    {"id":127,"name":"Ankit","dept":"MCA"},
+    {"id":128,"name":"Sumit","dept":"MBA"}
+  ];
+   getColor(branch){
+        switch (branch) {
+          case 'MCA':
+            return 'green';
+          case 'MBA':
+            return 'red';
+          case 'MTech':
+            return 'blue'; 
+          default:
+            break;
+        }
+  }
+
 }
